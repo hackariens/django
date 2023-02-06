db10d3f6562af0e7cebd7931eb11577d673ceb90
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2023-02-06 10:59:40 +0100

    test(docker) correction install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python -m venv .venv
-source .venv/bin/activate
+python -m venv /.venv
+source /.venv/bin/activate
 python -m pip install django~=4.0.2
 django-admin startproject apps
-pip freeze > apps/requirements.txt
+pip freeze > /app/requirements.txt
